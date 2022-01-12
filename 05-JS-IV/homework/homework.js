53880f294db5d5a35845e1a1eebe87056d6d4e3d
esusanj <estefaniasusanj@gmail.com>
2022-01-12 01:03:36 -0300

    tarea

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -99,7 +99,7 @@ function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
-  usuario.password= nuevaPassword;
+  usuario.password= nuevaPassword; 
   return usuario;
 }
 
